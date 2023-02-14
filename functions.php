8d7511a079ec7501e69a1f73af3f520cc673d562
JoleneDwwm <jolene.dwwm2022@gmail.com>
2023-02-14 10:46:53 +0100

    functions.php update

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+function montheme_supports(){
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_theme_support('menus');
+    register_nav_menu('header', 'En tête du menu');
+    register_nav_menu('footer', 'Pied de page');
+
+    add_image_size('post-thumbnail', 350, 215, true);
+}
+add_action('after_setup_theme', 'montheme_supports');
